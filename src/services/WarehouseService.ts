d1ecc20f21f9f8f4f89edf2e8d1a3eeada6a6b33
PHTLing <145812848+PHTLing@users.noreply.github.com>
2024-10-11 21:42:21 +0700

    Refactor WarehouseService import statements

--- src/services/WarehouseService.ts
+++ src/services/WarehouseService.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { InjectModel } from '@nestjs/mongoose';
+import {  } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { WarehouseInterface, WarehouseModal } from '../models/Warehouse';
 
