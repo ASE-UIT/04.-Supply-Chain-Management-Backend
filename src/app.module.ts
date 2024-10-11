3237e998cb3243221c42f0f3071fd1155f209f37
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-10-11 17:55:58 +0700

    revamp database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,18 @@
-import { AuthModule } from '@gstb/modules/AuthModule';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-// import { DocumentModule } from '@gstb/modules/DocumentModule';
-// import { DeviceCategoryModule } from './modules/DeviceCategoryModule';
-// import { DeviceModule } from './modules/DeviceModule';
-// import { MaintainanceHistoryModule } from './modules/MaintainanceHistoryModule';
-// import { UsageHistoryModule } from './modules/UsageHistoryModule';
-// import { UpdateScheduleModule } from './modules/UpdateScheduleModule';
-import { UserModule } from './modules/UserModule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['local.env', 'staging.env', 'production.env'],
-      isGlobal: true,
+      envFilePath: '.development.env',
     }),
-    MongooseModule.forRoot(process.env.DATABASE_HOST),
-    AuthModule,
-    UserModule,
-    // DocumentModule,
-    // DeviceCategoryModule,
-    // DeviceModule,
-    // MaintainanceHistoryModule,
-    // UsageHistoryModule,
-    // UpdateScheduleModule,
-  ]
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule { }
+export class AppModule {}
